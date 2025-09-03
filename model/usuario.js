0f5c011dcd48e18b0a0fc016218feeba040b5da4
LeonardoRomulo <leo.romulo15@gmail.com>
2025-09-03 17:31:22 -0300

    feat: Configurando a conexão com o banco de dados e as variáveis de ambiente

---
+++ model/usuario.js
@@ -0,0 +1,10 @@
+class Usuario {
+    constructor (nome,email, cpf, senha){
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.senha = senha;
+    };
+}
+
+export default Usuario;

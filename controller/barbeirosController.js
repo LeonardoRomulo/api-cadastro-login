89b8b300d561214eb8bf0835ea255d621923fea4
LeonardoRomulo <leo.romulo15@gmail.com>
2025-09-11 09:33:04 -0300

    fix(controller): correção de erros de digitação e lógica

--- controller/barbeirosController.js
+++ controller/barbeirosController.js
@@ -7,13 +7,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 class BarbeirosController {
-    static async criarBarbeiros(req, res) {
+    static async criarBarbeiro(req, res) {
         try {
             const { nome, email, senha, especialidade, is_adm, foto } = req.body;
             const senhaHash = await bcrypt.hash(senha, 10);
-            const barbeiro = new Barbeiro(nome, email, senha, especialidade, is_adm, foto);
+            const barbeiro = new Barbeiro(nome, email, senhaHash, especialidade, is_adm, foto);
             const query = "INSERT INTO barbeiros (nome, email, senha, especialidade, is_adm, foto) VALUES (?, ?, ?, ?, ?, ?)";
-            await conexao.query(query, [barbeiro.nome, barbeiro.email, barbeiro.senha, barbeiro.senha, barbeiro.especialidades, barbeiro.is_adm, barbeiro.foto]);
+            await conexao.query(query, [barbeiro.nome, barbeiro.email, barbeiro.senha, barbeiro.especialidade, barbeiro.is_adm, barbeiro.foto]);
             return res.status(200).json({ message: "Cadastro criado com sucesso" });
         } catch (err) {
             return res.status(500).json({ message: err.message });

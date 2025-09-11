b5fc2e70c9f8a8cce1495bfeabfa8eba178436e0
LeonardoRomulo <leo.romulo15@gmail.com>
2025-09-11 17:20:00 -0300

    refactor(controller): Corrigido a indentação do arquivo

--- controller/barbeirosController.js
+++ controller/barbeirosController.js
@@ -12,9 +12,9 @@ class BarbeirosController {
         try {
             const { nome, email, senha, especialidade, is_adm, foto } = req.body;
             //Verifica se o email já existe no bd
-            const [existe] = await conexao.query('SELECT id FROM barbeiros WHERE email = ?',[email]);
-            if(existe.length > 0){
-                return res.status(409).json({message: "Email já existe"});
+            const [existe] = await conexao.query('SELECT id FROM barbeiros WHERE email = ?', [email]);
+            if (existe.length > 0) {
+                return res.status(409).json({ message: "Email já existe" });
             }
             const senhaHash = await bcrypt.hash(senha, 10);
             const barbeiro = new Barbeiro(nome, email, senhaHash, especialidade, is_adm, foto);
@@ -44,21 +44,21 @@ class BarbeirosController {
             //Execução da query, caso haja correspodencia entre o email do cliente e do bd pega a primeira aparição e salva na costante resultado
             const [resultado] = await conexao.query(query, [email]);
             // validação do email caso não haja correspodência ele retorna a mensagem de erro abaixo
-            if(resultado.length === 0){
-                return res.status(401).json({ error: "Email ou senha inválida"});
+            if (resultado.length === 0) {
+                return res.status(401).json({ error: "Email ou senha inválida" });
             }
 
             //autenticação da senha
 
             //Captura o primeiro barbeiro no bd que o email informado pelo cliente corresponda com o bd
-            const barbeiro =  resultado[0];
-            
+            const barbeiro = resultado[0];
+
             //Compara a senha digitada pelo cliente com o hash da senha no bd usando o bcrypt
             const senhaValida = await bcrypt.compare(senha, barbeiro.senha);
 
             //valida a credencial da senha
-            if(!senhaValida){
-                return res.status(401).json({ error: "Email ou senha inválida"});
+            if (!senhaValida) {
+                return res.status(401).json({ error: "Email ou senha inválida" });
             };
 
             //Configuração do token jwt
@@ -66,9 +66,9 @@ class BarbeirosController {
             const segredo = process.env.JWT_SECRET;
 
             const token = jwt.sign(
-                {id: barbeiro.id, email: barbeiro.email},
+                { id: barbeiro.id, email: barbeiro.email },
                 segredo,
-                {expiresIn: "1h"}
+                { expiresIn: "1h" }
             );
 
             return res.status(200).json({ message: "Barbeiro logado com sucesso", token });
@@ -89,34 +89,34 @@ class BarbeirosController {
     };
 
     //metódo para atualizar os dados dos barbeiros
-    static async atualizarBarbeiros(req,res){// utilizar a rota patch
+    static async atualizarBarbeiros(req, res) {// utilizar a rota patch
         try {
-            const {id} = req.params; //obtem o id do barbeiro que deseja ser atualizado
+            const { id } = req.params; //obtem o id do barbeiro que deseja ser atualizado
             const { nome, email, senha, especialidade, foto } = req.body; // pega os dados informados pelo cliente no corpo da requisição
 
-            const camposBody = {nome, email, especialidade, foto}; //Cria um objeto com os dados informados pelo cliente 
+            const camposBody = { nome, email, especialidade, foto }; //Cria um objeto com os dados informados pelo cliente 
             //monta um array vazio  nas variáveis campos e valores para receber os campos e os valores que vão ser atualizados de forma dinâmica
-            let campos =[]; 
-            let valores =[];
+            let campos = [];
+            let valores = [];
 
             //loop  que usa o metodo nativo do js object.entries que obtem os pares campo e valor que são percorridos pelo loop e adiciona o nome do campo no array campos e o valor no array valores caso caso esse não seja indefenido e atualiza só os campos informados de forma dinâmica.
-            for(const[campo, valor] of Object.entries(camposBody)){
-                if(valor !=='undefined') {
+            for (const [campo, valor] of Object.entries(camposBody)) {
+                if (valor !== 'undefined') {
                     campos.push(`${campo} = ?`);
                     valores.push(valor);
                 }
             }
 
             //atualização da senha, o usuário atualiza a senha, ela é criptografada e depois atualizada no bd
-            if(senha){
-                const senhaHash = await bcrypt.hash(senha,10);
+            if (senha) {
+                const senhaHash = await bcrypt.hash(senha, 10);
                 campos.push("senha = ?");
                 valores.push(senhaHash)
             };
 
             //Condicional para verificar se o array campos está vazio se tiver ele retrona o status 400 e a mensagem de erro
-            if(campos.length === 0) {
-                return res.status(400).json({message: "Nenhum campo para atualizar"});
+            if (campos.length === 0) {
+                return res.status(400).json({ message: "Nenhum campo para atualizar" });
             };
             // adiciona o id que será ataulizado no fim do array valores
             valores.push(id)
@@ -125,35 +125,35 @@ class BarbeirosController {
             await conexao.query(query, valores)
 
             //retorno da mensagem de sucesso
-           return res.status(200).json({message: "Barbeiro atualizado com sucesso"});
+            return res.status(200).json({ message: "Barbeiro atualizado com sucesso" });
         } catch (err) {
             //retorno da mensagem de erro
-            res.status(500).json({message: err.message});
+            res.status(500).json({ message: err.message });
         }
     }
 
     //Metódo para deletar um barbeiro
-    static async deletarBarbeiro(req,res){
-        try{
-            const {id} = req.params;
+    static async deletarBarbeiro(req, res) {
+        try {
+            const { id } = req.params;
 
             //Validando se o id foi informado
-            if(!id){
-                return res.status(400).json({message: "ID não informado"});
+            if (!id) {
+                return res.status(400).json({ message: "ID não informado" });
             }
 
             //Verifica se o barbeiro existe
 
             const [existe] = await conexao.query('SELECT id FROM barbeiros WHERE id = ?', [id]);
-            if(existe.length === 0) {
-                return res.status(400).json({message: "Barbeiro não encontrado"});
+            if (existe.length === 0) {
+                return res.status(400).json({ message: "Barbeiro não encontrado" });
             }
 
             const query = 'DELETE FROM barbeiros WHERE id = ?';
             await conexao.query(query, [id]);
-            return res.status(200).json({message: "Barbeiro deletado com sucesso"});
-        }catch(err){
-            return res.status(500).json({message: err.message});
+            return res.status(200).json({ message: "Barbeiro deletado com sucesso" });
+        } catch (err) {
+            return res.status(500).json({ message: err.message });
         }
     }
 }

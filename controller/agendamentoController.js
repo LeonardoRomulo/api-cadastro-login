f67515124ce276ae5d85594c8bc08b76471b5257
LeonardoRomulo <leo.romulo15@gmail.com>
2025-09-13 17:55:40 -0300

    feat(model): Implementado a entidade Agendamentos

---
+++ controller/agendamentoController.js
@@ -0,0 +1,22 @@
+import conexao from "../model/conexao.js";
+import Agendamento from "../model/agendamento.js";
+
+class AgendamentoController {
+    static async criarAgendamento(req, res) {
+        // Implementação futura
+    }
+
+    static async listarAgendamentos(req, res) {
+        // Implementação futura
+    }
+
+    static async atualizarAgendamento(req, res) {
+        // Implementação futura
+    }
+
+    static async deletarAgendamento(req, res) {
+        // Implementação futura
+    }
+}
+
+export default AgendamentoController;
